f808bfdb2b4e78e8466b2b4100aec2e89233aa1b
HARSH SINGH <131766995+dsharshthakur@users.noreply.github.com>
2024-10-06 03:18:15 +0530

    Update model.py

--- model.py
+++ model.py
@@ -106,7 +106,7 @@ class Workflow:
         if self.vector_db != "":
             db = FAISS.load_local(self.vector_db, embeddings=tokenizer, allow_dangerous_deserialization=True)
 
-            thresh = 0.67
+            thresh = 0.75
             res = db.similarity_search_with_score(question, k=195)
 
             recommendations = []
